eb9e11a5015d8c5ef1352c52f6d6538f1c44e722
FellipeLorram <83411249+FellipeLorram@users.noreply.github.com>
2021-09-09 08:25:34 -0300

    Add files via upload

---
+++ code/verMais.js
@@ -0,0 +1,85 @@
+export const VerMaisArmacoes = {
+    open() {
+        const html = `
+        <section class="ver_mais_container">
+        <div class="ver_mais_container--header">
+            <span id="btn_close" class="material-icons-outlined">close</span>
+            <div class="filter__button_container">
+                <div class="filter__button">
+                    <span class="material-icons-outlined">filter_alt</span>
+                    <span class="filter__button-text">Filtrar</span>
+                </div>
+            </div>
+        </div>
+        <div class="filters__container">
+        </div>
+        <div class="container">
+            <div class="informative_text">
+                <div class="informative_text--container">
+                    <p>
+                        Todas as nossas armações são em materiais selecionados, como Acetato comum, Acetato
+                        mazzucchelli,
+                        Aluminio, Metal.<br>
+                        Assim como marcas selecionadas, como Ana Hickman, Menina Bela, Oakley, Nike, Lacoste, Vogue e
+                        mais.
+                        Não trabalhamos com materias de brinquedo, como o plastico, por exemplo.
+                    </p>
+                    <div class="informative_text--close">
+                        <span class="material-icons-outlined">close</span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="ver_mais_content">
+                <div class="foto_holder">
+                    <img src="./style/images/IMG_20210414_095209.jpg" alt="armacao lacoste">
+                    <div class="img_from">
+                        <a href="https://www.instagram.com/oticagirehoficial/">@oticagirehoficial</a>
+                    </div>
+                    <div class="img_info">
+                        <p>Armação em acetato Lacoste.</p>
+                        <p class="price">R$280</p>
+                    </div>
+                </div>
+                <div class="foto_holder">
+                    <img src="./style/images/IMG_20210414_095209.jpg" alt="armacao lacoste">
+                    <div class="img_from">
+                        <a href="https://www.instagram.com/oticagirehoficial/">@oticagirehoficial</a>
+                    </div>
+                    <div class="img_info">
+                        <p>Armação em acetato Lacoste.</p>
+                        <p class="price">R$280</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        `;
+
+        const template = document.createElement("template");
+        template.innerHTML = html;
+
+
+        const windownContainer = template.content.querySelector(".ver_mais_container");
+        const btnClose = template.content.querySelector("#btn_close");
+        const btnCloseInformativeText = template.content.querySelector(".informative_text--close");
+
+        btnClose.addEventListener('click', () => {
+            this._close(windownContainer);
+        });
+
+        btnCloseInformativeText.addEventListener('click', () => document.querySelector('.informative_text').style.display = 'none');
+
+        document.body.appendChild(template.content);
+        document.body.classList.add('stop-scrolling');
+    },
+
+    _close(windownContainer) {
+        windownContainer.classList.add('confirm-close');
+
+        windownContainer.addEventListener('animationend', () => {
+            document.body.removeChild(windownContainer)
+            document.body.classList.remove('stop-scrolling')
+        })
+    },
+}
