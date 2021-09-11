2df59c548f408058da7e69c4fafaa1c8ae41ddd1
FellipeLorram <83411249+FellipeLorram@users.noreply.github.com>
2021-09-11 11:04:06 -0300

    Update filterOption.js

--- code/filterOption.js
+++ code/filterOption.js
@@ -8,8 +8,9 @@ export const FilterOptions = {
                 </div>
                 <div class="filters_body">
                     <div class="mas_fem">
+                        <div class="todas selected">Todas</div>
                         <div class="masculino">Masculino</div>
-                        <div class="Feminino">Feminino</div>
+                        <div class="feminino">Feminino</div>
                     </div>
                     <div class="marcas__container">
                         <div class="marcas--header">
@@ -24,6 +25,14 @@ export const FilterOptions = {
                             <div class="marca">Fridon</div>
                             <div class="marca">Ray-Ban</div>
                             <div class="marca">Puma</div>
+                            <div class="marca">Menina Bela</div>
+                            <div class="marca">Vogue</div>
+                            <div class="marca">Carolina Herreira</div>
+                            <div class="marca">Tiffany CO.</div>
+                            <div class="marca">Dulcet</div>
+                            <div class="marca">Fridon</div>
+                            <div class="marca">Ray-Ban</div>
+                            <div class="marca">Dolce & Gabbana</div>
                         </div>
                         <div class="marcas--footer">
                             <button>Aplicar</button>
@@ -41,12 +50,31 @@ export const FilterOptions = {
 
         const windownContainer = template.content.querySelector(".filters_windown_container");
         const btnClose = template.content.querySelector(".filters_header span");
+        const btnMasc = template.content.querySelector(".masculino");
+        const btnFem = template.content.querySelector(".feminino");
+        const btnTds = template.content.querySelector(".todas");
+        const btnAplicar = template.content.querySelector(".marcas--footer button");
 
         btnClose.addEventListener('click', () => {
             this._close(windownContainer);
         });
 
+        [btnTds, btnFem, btnMasc].forEach(btn => btn.addEventListener('click', e => {
+            [btnTds, btnFem, btnMasc].forEach(RemoveClass => RemoveClass.classList.remove('selected'));
+
+            e.target.classList.add('selected')
+            this._handleOptionFilters(e.target, btnMarcas)
+        }));
+
         document.body.appendChild(template.content);
+
+        const btnMarcas = document.querySelectorAll('.marca');
+        btnMarcas.forEach(btn => btn.addEventListener('click', e => e.target.classList.toggle('selected')));
+
+        btnAplicar.addEventListener('click', () => {
+            this._Filter(windownContainer);
+        });
+
     },
 
     _close(windownContainer) {
@@ -58,4 +86,64 @@ export const FilterOptions = {
         })
     },
 
+    _handleOptionFilters(e) {
+        if (e.classList.contains('feminino') && e.classList.contains('selected')) {
+            document.querySelector('.marcas-content').innerHTML = `
+            <div class="marca">Menina Bela</div>
+            <div class="marca">Vogue</div>
+            <div class="marca">Carolina Herreira</div>
+            <div class="marca">Tiffany CO.</div>
+            <div class="marca">Dulcet</div>
+            <div class="marca">Fridon</div>
+            <div class="marca">Ray-Ban</div>
+            <div class="marca">Dolce & Gabbana</div>
+            `;
+
+
+        }
+        if (e.classList.contains('masculino') && e.classList.contains('selected')) {
+            document.querySelector('.marcas-content').innerHTML = `
+            <div class="marca">Life Glasses</div>
+            <div class="marca">Oakley</div>
+            <div class="marca">Nike</div>
+            <div class="marca">Lacoste</div>
+            <div class="marca">Dulcet</div>
+            <div class="marca">Fridon</div>
+            <div class="marca">Ray-Ban</div>
+            <div class="marca">Puma</div>
+            `;
+        }
+        if (e.classList.contains('todas') && e.classList.contains('selected')) {
+            document.querySelector('.marcas-content').innerHTML = `
+            <div class="marca">Life Glasses</div>
+            <div class="marca">Oakley</div>
+            <div class="marca">Nike</div>
+            <div class="marca">Lacoste</div>
+            <div class="marca">Dulcet</div>
+            <div class="marca">Fridon</div>
+            <div class="marca">Ray-Ban</div>
+            <div class="marca">Puma</div>
+            <div class="marca">Menina Bela</div>
+            <div class="marca">Vogue</div>
+            <div class="marca">Carolina Herreira</div>
+            <div class="marca">Tiffany CO.</div>
+            <div class="marca">Dulcet</div>
+            <div class="marca">Fridon</div>
+            <div class="marca">Ray-Ban</div>
+            <div class="marca">Dolce & Gabbana</div>
+            `;
+        }
+
+        const btnMarcas = document.querySelectorAll('.marca');
+        btnMarcas.forEach(btn => btn.addEventListener('click', e => e.target.classList.toggle('selected')));
+    },
+
+    _Filter(windownContainer) {
+        const sex = document.querySelector('.mas_fem div.selected');
+        const fotos = document.querySelectorAll('.foto_holder');
+        const marcasSelecteds = document.querySelectorAll('.marca.selected');
+        const fotoContainer = document.querySelector('.ver_mais_content')
+
+        this._close(windownContainer);
+    }
 }
