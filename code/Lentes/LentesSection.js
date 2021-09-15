ddb56f778a3f968df64cfd362f12af561043ec02
FellipeLorram <83411249+FellipeLorram@users.noreply.github.com>
2021-09-15 07:09:35 -0300

    Update LentesSection.js

--- code/Lentes/LentesSection.js
+++ code/Lentes/LentesSection.js
@@ -82,58 +82,75 @@ export const VerMaisLentes = {
 
     _BoaLente() {
         return `
-            <p>Um material adequado, 
-            tratamentos que combinem com seu dia a dia e com estilo da sua armação, 
-            são características de uma boa lente.</p>
-                        
-            <h1>O Material Adequado</h1>
-            <p>Cada armação pede um tipo de material de lente.</p>
-            <p> 
-            <b>Armações Abertas e Armações balgriff:</b><br><br>
-            Por esse tipo de armação não proteger totalmente a lente, o material adequado seria o mais resistente possível, como o policarbonato,
-            resina 1.61, resina 1.67.<br><br>
-                            
-            Armações abertas geralmente seguram as lentes com a ajuda de um fio de nylon. Um friso é feito em volta de toda a lente, onde passa o 
-            nylon. Para aguentar esse friso, a lente precisa ser bastante resistente, pois se não, corre-se o risco de quebrar na hora da montagem.<br><br>
-            </p> 
-            <div class="img_container_lentes_section">
-                <span>Armação Aberta</span>
-                <img class="img_lentes_section" src="./code/Armacoes/images/oakleyAbertaPRosa.jpeg" alt="armacao"><br>
-            </div>
-            <p> 
-            Já as Armações balgriff, 
-            seguram a lente através de parafusos fixados na própia lente, 
-            causando assim a necessidade da sua resistência.
-            </p>
-            <div class="img_container_lentes_section">
-                <span>Armação Balgriff</span>
-                <img class="img_lentes_section" src="./code/Armacoes/images/oakleyAbertaPRosa.jpeg" alt="armacao"><br>
-            </div>
-            <p><b>Armações fechadas:</b><br><br>
-            Esse tipo de armação suporta todos os materiais de lente. 
-            Por serem totalmente fechadas, essas armações oferecem uma proteção a mais para a lente, já que as mesmas não correm o risco de 
-            acabarem batendo, ou esbarrando suas bordas. <br><br>
-            
-            Lentes em resina comum (1.56) são as mais utilizadas nos casos de armações fechadas. <br><br>Existem outros fatores, como o 
-            seu grau em relação ao tamanho de sua armação, que alterariam o tipo de sua lente. 
+        <p>Um material adequado,
+        tratamentos que combinem com seu dia a dia e com estilo da sua armação,
+        são características de uma boa lente.</p>
 
-            </p>
-            <div class="img_container_lentes_section b">
-                <div class="img">
-                <img class="img_lentes_section" src="./code/Armacoes/images/oakleyPretaAberta.jpeg" alt="armacao">
-                <p>Armação aberta em metal</p>
-                </div>
-                <div class="img">
-                <img class="img_lentes_section" src="./code/Armacoes/images/DulcetRedondaAzul.jpeg" alt="armacao">
-                <p>Armação aberta em acetato</p>
-                </div>
-            </div>
-            
-            <p>
-                Caso você tenha um grau alto de miopia, por exemplo
-            </p>
-                        
-            <h1>O Tratamento para você</h1>
+    <h1>O Material Adequado</h1>
+    <p>Cada armação pede um tipo de material de lente.</p>
+    <p>
+        <b>Armações Abertas e Armações balgriff:</b><br><br>
+        Por esse tipo de armação não proteger totalmente a lente, o material adequado seria o mais resistente
+        possível, como o policarbonato,
+        resina 1.61, resina 1.67.<br><br>
+
+        Armações abertas geralmente seguram as lentes com a ajuda de um fio de nylon. Um friso é feito em volta
+        de toda a lente, onde passa o
+        nylon. Para aguentar esse friso, a lente precisa ser bastante resistente, pois se não, corre-se o risco
+        de quebrar na hora da montagem.<br><br>
+    </p>
+
+    <div class="img_container_lentes_section b">
+        <div class="img">
+            <img class="img_lentes_section" src="./code/Armacoes/images/oakleyPretaAberta.jpeg" alt="armacao">
+            <p>Armação aberta</p>
+        </div>
+        <div class="img">
+            <img class="img_lentes_section" src="./code/Armacoes/images/oakleyPretaAberta.jpeg" alt="armacao">
+            <p>Armação balgriff</p>
+        </div>
+    </div>
+
+    <p>
+        Já as Armações balgriff,
+        seguram a lente através de parafusos fixados na própia lente,
+        causando assim a necessidade da sua resistência.
+    </p>
+
+    <p><b>Armações fechadas:</b><br><br>
+        Esse tipo de armação suporta todos os materiais de lente.
+        Por serem totalmente fechadas, essas armações oferecem uma proteção a mais para a lente, já que as
+        mesmas não correm o risco de
+        acabarem batendo, ou esbarrando suas bordas. <br><br>
+
+        Lentes em resina comum (1.56) são as mais utilizadas nos casos de armações fechadas. <br><br>
+
+    </p>
+    <div class="img_container_lentes_section b">
+        <div class="img">
+            <img class="img_lentes_section" src="./code/Armacoes/images/oakleyPretaAberta.jpeg" alt="armacao">
+            <p>Armação fechada em metal</p>
+        </div>
+        <div class="img">
+            <img class="img_lentes_section" src="./code/Armacoes/images/DulcetRedondaAzul.jpeg" alt="armacao">
+            <p>Armação fechada em acetato</p>
+        </div>
+    </div>
+
+    <p>
+        Existem outros fatores, como o seu grau em relação ao tamanho de sua armação, que alterariam o tipo de sua lente.
+        Caso você tenha um grau alto de miopia, por exemplo, já não é aconselhavel a escolha de armações grandes.
+    </p>
+
+    <h1>O Tratamento para você</h1>
+    <p>
+        Tratamentos são como adicionais que colocamos na lente, camadas a mais de proteção, ou, que ajudam na estética do seu óculos.
+        <b class="minor_text">(Para mais detalhes olhe a seção de tratamentos disponiveis.)</b><br>
+    </p>
+
+    <p>A primeira pergunta que você deve se fazer é, "como é o meu dia a dia?" Você fica muito no celular, computador, ou aparelhos que geram luz artificial?
+        Você fica muito tempo exposto a luz do sol?    
+    </p>
         `;
     }
 }
